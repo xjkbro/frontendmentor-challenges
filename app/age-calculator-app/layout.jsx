60c219f859b79d0e18ee10a0bcdbfd13e7cd9082
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-05-12 11:30:12 -0700

    metadata

---
+++ app/age-calculator-app/layout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export const metadata = {
+    title: "Age Calculator App",
+    description: "An Age Calculator App provided by Frontend Mentor",
+};
+export default function AgeLayout({ children }) {
+    return <>{children}</>;
+}
